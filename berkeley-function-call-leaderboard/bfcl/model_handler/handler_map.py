3e0451e449c75cb604d71e9ef772decd641d80af
Ayman Tarig <aymantarig17@gmail.com>
2024-12-17 14:33:14 +0200

    add AymanTarig/Llama-3.2-1B-FC-v1.11

--- berkeley-function-call-leaderboard/bfcl/model_handler/handler_map.py
+++ berkeley-function-call-leaderboard/bfcl/model_handler/handler_map.py
@@ -106,6 +106,7 @@ local_inference_handler_map = {
     "AymanTarig/Llama-3.2-1B-FC-v1.8": LlamaJsonHandler,
     "AymanTarig/Llama-3.2-1B-FC-v1.9": LlamaJsonHandler,
     "AymanTarig/Llama-3.2-1B-FC-v1.10": LlamaJsonHandler,
+    "AymanTarig/Llama-3.2-1B-FC-v1.11": LlamaJsonHandler,
     "AymanTarig/deepseek-coder-1.3b-FC-v1": DeepseekJsonHandler,
     "AymanTarig/deepseek-coder-1.3b-FC-v2": DeepseekJsonHandler,
     "deepseek-ai/deepseek-coder-1.3b-instruct": DeepseekJsonHandler,
