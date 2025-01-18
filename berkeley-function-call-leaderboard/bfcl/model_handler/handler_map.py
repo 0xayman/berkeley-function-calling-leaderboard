b14ac095bd5d95b1df6951914748da8ac97fd678
Ayman Tarig <aymantarig17@gmail.com>
2025-01-18 17:13:27 +0200

    AymanTarig/Llama-3.2-1B-FC-v0.3 update

--- berkeley-function-call-leaderboard/bfcl/model_handler/handler_map.py
+++ berkeley-function-call-leaderboard/bfcl/model_handler/handler_map.py
@@ -104,7 +104,7 @@ api_inference_handler_map = {
 local_inference_handler_map = {
     # Custom Models
     "AymanTarig/Llama-3.2-1B-FC-v0.2": LlamaJsonHandler,
-    "AymanTarig/Llama-3.2-1B-FC-v0.3": LlamaJsonHandler,
+    "AymanTarig/Llama-3.2-1B-FC-v0.3": LlamaFCHandler,
     "AymanTarig/Llama-3.2-1B-FC-v0.4": LlamaFCHandler,
     
     "google/gemma-2-2b-it": GemmaHandler,
